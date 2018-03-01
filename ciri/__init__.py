5564be9a212319509f4174158b9e1f77621a9053
Eric Bobbitt <eric@hellouser.net>
2018-02-28 20:40:08 -0600

    add license

--- ciri/__init__.py
+++ ciri/__init__.py
@@ -1,2 +1,5 @@
 from ciri.core import Schema, SchemaOptions, PolySchema
 from ciri.exception import ValidationError
+
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
-from setuptools import setup
+#!/usr/bin/env python
+import codecs
+import os
+import re
+from setuptools import setup, find_packages
 
-setup(name='ciri',
-      version='0.1.0',
-      description='Python Object Serialization Library',
-      url='https://github.com/ericb/ciri',
-      author='Eric Bobbitt <eric@hellosuer.net>, Daniel Imhoff <dwieeb@gmail.com>',
-      author_email='eric@hellouser.net')
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='ciri',
+    version=find_version('ciri', '__init__.py'),
+    description='Python Schema Library',
+    url='https://github.com/ericb/ciri',
+    author='Eric Bobbitt <eric@hellosuer.net>, Daniel Imhoff <dwieeb@gmail.com>',
+    author_email='eric@hellouser.net'
+    packages=find_packages(exclude=('test*', 'docs*')),
+    package_dir={'ciri': 'ciri'},
+    include_package_data=True,
+    license='MIT'
+)
